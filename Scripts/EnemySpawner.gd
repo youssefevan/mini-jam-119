9778960a0e6a86ea38b72f1cb7914db6f12df651
Evan <eyoussef128@gmail.com>
2022-11-12 17:26:38 -0500

    Spawn offset

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -5,9 +5,6 @@ export var game_manager_node: NodePath
 onready var game_manager = get_node(game_manager_node)
 var random = RandomNumberGenerator.new()
 
-export var movement_range := 20.0
-export var speed := 20.0
-
 var min_spawn_rate := .3
 var max_spawn_rate := 2.0
 var min_spawn_rate_modifier := 0.8 # decreases min spawn by 20% per wave
@@ -16,7 +13,6 @@ var spawn_wait: float
 var spawn: bool
 
 func _ready():
-	move()
 	game_manager.connect("change_wave", self ,"_on_change_wave")
 	spawn = true
 
@@ -27,29 +23,16 @@ func _physics_process(delta):
 	if spawn == true:
 		spawn_enemy()
 
-func move():
-	var tween = get_node("Tween")
-	tween.interpolate_property(self, "position",
-			position, position + Vector2(movement_range, 0), .5,
-			Tween.TRANS_LINEAR)
-	tween.start()
-
 func spawn_enemy():
 	spawn = false
 	yield(get_tree().create_timer(spawn_wait), "timeout")
 	
 	var enemy = ememy_type1.instance()
-	enemy.position = global_position
+	var spawn_offset = random.randf_range(-50, 50)
+	enemy.position = global_position + Vector2(spawn_offset, 0)
 	get_tree().get_root().call_deferred("add_child", enemy)
 	
 	spawn = true
 
 func _on_change_wave():
 	min_spawn_rate *= min_spawn_rate_modifier
-
-func _on_Tween_tween_completed(object, key):
-	var tween = get_node("Tween")
-	tween.interpolate_property(self, "position",
-			position, position + Vector2(-movement_range, 0), .5,
-			Tween.TRANS_LINEAR)
-	tween.start()
