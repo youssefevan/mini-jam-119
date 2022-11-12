7066c4a5e00e2806047c5e99a11f533b7d157c6f
8by4 <aidanquinn147@gmail.com>
2022-11-12 17:39:25 -0500

    Tower3 Update

--- Path2D.gd
+++
@@ -1,9 +0,0 @@
-extends Path2D
-
-var inc=0
-var speed=500
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	inc+=delta*speed
-	$PathFollow2D.offset=inc
