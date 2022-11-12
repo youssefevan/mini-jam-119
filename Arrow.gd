1b5fa6a21b04f9dfa0b2d79d805b31877421f838
8by4 <aidanquinn147@gmail.com>
2022-11-11 22:55:42 -0500

    FINALLY SHOOTER!

--- Arrow.gd
+++ Arrow.gd
@@ -1,6 +1,13 @@
 extends Area2D
 
+# Declare member variables here. Examples:
+export var speed = 750
+var velocity = Vector2()
 
 
-func _ready():
-	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position += transform.x * speed * delta
+	#this is to make the bullet fly to the right when fired
+
--- Scripts/Tower.gd
+++ Scripts/Tower.gd
@@ -3,17 +3,37 @@ extends KinematicBody2D
 var current: bool
 var towerNumber: int
 
+
 func _ready():
 	Global.numberOfTowers += 1
 	towerNumber = Global.numberOfTowers
-
+	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("select1"):
 		current = true
 	
 	if current == true:
 		aim()
-
+		shoot()
+	
+	print($BowAxis/Bow.global_position)
+	
 func aim():
 	$BowAxis.look_at(get_global_mouse_position())
+	
+func shoot():
+	if Input.is_action_just_pressed("fire"):
+		var arrow = load("res://Arrow.tscn").instance()
+		
+		arrow.rotation = $BowAxis.global_rotation
+		arrow.position = $BowAxis/Bow.global_position - ($BowAxis.global_position)
+		#stupid position setter
+#		arrow.position.x = (position.x*cos(arrow.rotation))
+#		arrow.position.y = (position.y*sin(arrow.rotation))
+		
+#		arrow.position = $
+		
+		add_child(arrow)
+	
+	
 	
