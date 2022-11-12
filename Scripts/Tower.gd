51ac94c04ff021ce5c62fa25412e0b07f7d6c0c4
Evan <eyoussef128@gmail.com>
2022-11-11 20:50:44 -0500

    Tower buttons

--- Scripts/Tower.gd
+++ Scripts/Tower.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 
 var current: bool
+var towerNumber: int
 
 func _ready():
-	pass
+	Global.numberOfTowers += 1
+	towerNumber = Global.numberOfTowers
 
 func _physics_process(delta):
-	aim()
+	if Input.is_action_just_pressed("select1"):
+		current = true
+	
+	if current == true:
+		aim()
 
 func aim():
 	$BowAxis.look_at(get_global_mouse_position())
