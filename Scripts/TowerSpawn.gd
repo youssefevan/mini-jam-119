06275bd6ec8eef4df2563815107bc75c78eb9fd6
Evan <eyoussef128@gmail.com>
2022-11-11 21:46:22 -0500

    Can spawn tower at selected spawn location

--- Scripts/TowerSpawn.gd
+++ Scripts/TowerSpawn.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var tower_node = load("res://Scenes/Tower.tscn")
+
 export var spawnNumber: int
 var selected: bool
 
@@ -7,7 +9,10 @@ func _ready():
 	selected = false
 
 func _physics_process(delta):
-	pass
+	if selected and Input.is_action_just_pressed("select1"):
+		var tower = tower_node.instance()
+		add_child(tower)
+		tower.current = true
 
 func _on_TowerSpawn_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
