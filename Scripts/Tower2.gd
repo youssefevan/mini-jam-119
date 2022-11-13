58c745e665391502e08253328d0e15712c2a60ab
Evan <eyoussef128@gmail.com>
2022-11-12 23:59:59 -0500

    Auto fire and passive fire

--- Scripts/Tower2.gd
+++ Scripts/Tower2.gd
@@ -2,12 +2,21 @@ extends "res://Scripts/TowerBase.gd"
 
 export var health := 2
 
+export var active_fire_rate := 0.3
+export var passive_fire_rate := 0.5
+var can_shoot: bool
+
+func _ready():
+	can_shoot = true
+
 func _physics_process(delta):
 	if current == true:
 		aim()
-		
-		if Input.is_action_just_pressed("fire"):
+		if can_shoot:
 			shoot()
+		
+	elif can_shoot:
+		shoot()
 	
 	if (health <= 0):
 		call_deferred("free")
@@ -17,15 +26,23 @@ func aim():
 	flip()
 	
 func shoot():
-	if Input.is_action_just_pressed("fire"):
-		#$Animator.play("ShootAnim")
+	can_shoot = false
+	
+	#$Animator.play("ShootAnim")
 		
-		var orb = load("res://Scenes/LightOrb.tscn").instance()
+	var orb = load("res://Scenes/LightOrb.tscn").instance()
 		
-		orb.rotation = $BookAxis.global_rotation
-		orb.position = $BookAxis/Book.global_position - ($BookAxis.global_position)
+	orb.rotation = $BookAxis.global_rotation
+	orb.position = $BookAxis/Book.global_position - ($BookAxis.global_position)
 		
-		get_parent().add_child(orb)
+	get_parent().add_child(orb)
+		
+	if current == true:
+		yield(get_tree().create_timer(active_fire_rate), "timeout")
+		can_shoot = true
+	else:
+		yield(get_tree().create_timer(passive_fire_rate), "timeout")
+		can_shoot = true
 
 func _on_Hurtbox_body_entered(body):
 	if body.get_collision_layer() == 4:
