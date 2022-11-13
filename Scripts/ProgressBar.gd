ea746cc80fcba1ccff432de17c2784d2d4b39835
8by4 <aidanquinn147@gmail.com>
2022-11-13 18:28:42 -0500

    Progressbar crash fix

--- Scripts/ProgressBar.gd
+++ Scripts/ProgressBar.gd
@@ -8,7 +8,7 @@ extends ProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ProgressBar.value = 100
+	value = 100
 	pass # Replace with function body.
 
 
