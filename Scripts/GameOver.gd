b923d001b0f4c36e68a47499a3374d43c8b196fc
Evan <eyoussef128@gmail.com>
2022-11-13 19:30:22 -0500

    Game over update

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+func _input(event):
+	if Input.is_action_just_pressed("restart"):
+		Global.game_over = false
+		$DeleteAll.call_deferred("free")
+		get_tree().change_scene("res://Scenes/Stone.tscn")
+
 func _on_DeleteAll_body_entered(body):
 	body.call_deferred("free")
