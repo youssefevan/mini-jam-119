e811d0f8c3f96c2c1eec8ceadbab4bbfb2759c42
Evan <eyoussef128@gmail.com>
2022-11-13 20:28:37 -0500

    Title work

--- Scripts/Title Screen.gd
+++ Scripts/Title Screen.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-func _on_StartBtn_mouse_entered():
-	$StartBtn/Sprite.frame = 1
+func _on_Start_mouse_entered():
+	$Sprite.frame = 1
 
-func _on_StartBtn_mouse_exited():
-	$StartBtn/Sprite.frame = 0
+func _on_Start_mouse_exited():
+	$Sprite.frame = 0
 
-func _on_StartBtn_input_event(viewport, event, shape_idx):
-	if event.is_pressed():
-		get_tree().change_scene("res://Scenes/Stone.tscn")
+func _on_Start_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			get_tree().change_scene("res://Scenes/Stone.tscn")
